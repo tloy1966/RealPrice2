d2d83793e85dfcd32c74b888010bbbd64347d0ae
tloy1966 <tloy1966@gmail.com>
2017-06-18 13:05:35 +0800

    Try get api data

--- RealPrice/wwwroot/js/Heatmap.js
+++ RealPrice/wwwroot/js/Heatmap.js
@@ -1,5 +1,19 @@
-﻿var heatmapData = [
-  
+﻿
+var heatmapData = [
+  new google.maps.LatLng(37.782, -122.447),
+  new google.maps.LatLng(37.782, -122.445),
+  new google.maps.LatLng(37.782, -122.443),
+  new google.maps.LatLng(37.782, -122.441),
+  new google.maps.LatLng(37.782, -122.439),
+  new google.maps.LatLng(37.782, -122.437),
+  new google.maps.LatLng(37.782, -122.435),
+  new google.maps.LatLng(37.785, -122.447),
+  new google.maps.LatLng(37.785, -122.445),
+  new google.maps.LatLng(37.785, -122.443),
+  new google.maps.LatLng(37.785, -122.441),
+  new google.maps.LatLng(37.785, -122.439),
+  new google.maps.LatLng(37.785, -122.437),
+  new google.maps.LatLng(37.785, -122.435)
 ];
 
 var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
@@ -14,3 +28,8 @@ var heatmap = new google.maps.visualization.HeatmapLayer({
   data: heatmapData
 });
 heatmap.setMap(map);
+
+$.get('../api/Heatmap', function (data) {
+    console.log(data);
+    
+});
